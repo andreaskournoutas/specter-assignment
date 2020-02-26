836eb07c63c66ebd2e5703e80de4537fbfcbf213
Andreas Kournoutas <andreaskournoutas@gmail.com>
2020-02-26 21:30:07 +0200

    Files & folder structure

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>Specter assignment</title>
+        <link rel="stylesheet" href="css/style.css"/>
+    </head>
+    <body>
+        <?php include "components/header.php"; ?>
+
+        <?php include "components/footer.php"; ?>
+        <script src="js/script.js"></script>
+    </body>
+</html>
