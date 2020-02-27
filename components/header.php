01151ed271a05b1e239393e4e325320314179048
Andreas Kournoutas <andreaskournoutas@gmail.com>
2020-02-27 02:29:09 +0200

    Header component

---
+++ components/header.php
@@ -0,0 +1,25 @@
+<header class="header">
+    <div class="header__row header__row--top">
+        <div>
+            <img src="images/cloudevo-white.png"/>
+        </div>
+        <div class="header__burger-button">
+            <button class="burger-button">
+                <div class="burger-button__line burger-button__line--top"></div>
+                <div class="burger-button__line"></div>
+            </button>
+        </div>
+    </div>
+    <div class="header__row header__row--bottom">
+        <div>
+            <div class="header__title">We are<br/>Cloudevo</div>
+            <div class="header__subtitle">We help companies create digital dividends through cutting-edge creative and digital growth strategies.</div>
+        </div>
+        <div class="header__play-button-wrapper">
+            <img src="images/watch-reel.png"/>
+            <button class="header__play-button">
+                <img class="header__play-button-image" src="images/play-button.svg"/>
+            </button>
+        </div>
+    </div>
+</header>
