5fe110daa3833120fbe5231c5b41a600600d3821
Andreas Kournoutas <andreaskournoutas@gmail.com>
2020-03-01 17:19:45 +0200

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,14 @@
     </head>
     <body>
         <?php include "components/header.php"; ?>
-        <?php include "components/about.php"; ?>
+        <main>
+            <?php include "components/about.php"; ?>
+            <?php include "components/clients.php"; ?>
+            <?php include "components/about2.php"; ?>
+            <?php include "components/services.php"; ?>
+            <?php include "components/news.php"; ?>
+            <?php include "components/prompt.php"; ?>
+        </main>
         <?php include "components/footer.php"; ?>
         <script src="js/script.js"></script>
     </body>
